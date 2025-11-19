2135c47603a3b17bbe2f102bee07b54bed089df6
Tim Janik <timj@gnu.org>
2025-11-19 15:01:57 +0100

    cxx/imagewmark.hh: add CLI options for "add" command
    Signed-off-by: Tim Janik <timj@gnu.org>

--- cxx/imagewmark.hh
+++ cxx/imagewmark.hh
@@ -3,12 +3,28 @@
 #define __IMAGEWMARK_HH__
 
 #include "utils.hh"
+#include <CLI/CLI.hpp>
 
 struct Image {
   String comment;
   int    width = 0, height = 0, channels = 0;
   ByteS  pixels;
 };
+
+// CLI options for "add"
+struct AddOptions {
+  std::string cmd;
+  std::string input_img;
+  std::string output_img;
+  std::string message_hex;
+  float       strength       =  2.0;    // --strength
+  float       zoom           = -1.0;    // --zoom
+  float       dynamic_zoom   = -1.0;    // --dynamic-zoom
+  bool        trace_psnr     = false;   // --trace-psnr
+  bool        trace_quality  = false;   // --trace-quality
+};
+int       imagewmark_add         (const AddOptions &options);
+CLI::App* imagewmark_add_options (CLI::App &app, AddOptions &options);
 
 extern const char *argv0;
 
