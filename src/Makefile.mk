37374323abd831d2912a637c6282d94200ee124e
Tim Janik <timj@gnu.org>
2025-12-19 17:40:48 +0100

    src/Makefile.mk: check: add checks that use C++ embedding
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -4,7 +4,7 @@ src/watermark       := fedcba98765432100123456789abcdef
 
 # == src/check-{1024...} ==
 define CHECK_RESOLUTION
-src/check-$1:
+src/check-py-$1:
 	$(Q) echo '  CHECK   ' $$@
 	$(Q) convert tests/example01.svg -resize $1 $$@.png
 	$(Q) ./imagewmark add $$@.png $$@.wm.png $(src/watermark)
@@ -15,8 +15,21 @@ src/check-$1:
 		{ echo "$$@.png: failed to detect watermark" >&2 ; false ; }
 	$(Q) rm $$@.png $$@.wm.png $$@.json
 	$(Q) echo '  OK      ' $$@
-.PHONY: src/check-$1
-src/check: src/check-$1
+.PHONY: src/check-py-$1
+src/check: src/check-py-$1
+src/check-cxx-$1:
+	$(Q) echo '  CHECK   ' $$@
+	$(Q) convert tests/example01.svg -resize $1 $$@.png
+	$(Q) ./cxx/imagewmark-cxx add $$@.png $$@.wm.png $(src/watermark)
+	$(Q) test $1 -gt 2048 \
+	|| { convert $$@.wm.png -resize 1024 $$@.small.png && mv $$@.small.png $$@.wm.png ; }
+	$(Q) ./imagewmark get $$@.wm.png --json $$@.json
+	$(Q) grep -qE '\b$(src/watermark)\b' $$@.json || \
+		{ echo "$$@.png: failed to detect watermark" >&2 ; false ; }
+	$(Q) rm $$@.png $$@.wm.png $$@.json
+	$(Q) echo '  OK      ' $$@
+.PHONY: src/check-cxx-$1
+src/check: src/check-cxx-$1
 endef
 CHECK_RESOLUTIONS := 1024 2048 3072 4096 # 8192
 $(foreach R, $(CHECK_RESOLUTIONS), $(eval $(call CHECK_RESOLUTION,$R)))
