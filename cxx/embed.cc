441ba9b3ba77dcfe1893f0b7bab5913a624ccf08
Tim Janik <timj@gnu.org>
2026-09-14 22:04:30 +0200

    cxx/embed.cc: drop stale color profiles

--- cxx/embed.cc
+++ cxx/embed.cc
@@ -547,8 +547,10 @@ load_host_image (const std::string &path)
   const VipsInterpretation interpretation = host.interpretation();
   if (interpretation != VIPS_INTERPRETATION_B_W && interpretation != VIPS_INTERPRETATION_GREY16 &&
       interpretation != VIPS_INTERPRETATION_RGB && interpretation != VIPS_INTERPRETATION_RGB16 &&
-      interpretation != VIPS_INTERPRETATION_sRGB && interpretation != VIPS_INTERPRETATION_CMYK)
+      interpretation != VIPS_INTERPRETATION_sRGB && interpretation != VIPS_INTERPRETATION_CMYK) {
     host = host.colourspace (VIPS_INTERPRETATION_sRGB);
+    host.remove ("icc-profile-data");
+  }
   HostImage result;
   result.format = host.format();
   result.interpretation = host.interpretation();
